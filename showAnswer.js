ca252ee64facfce7fa44a3eebb06145ce0871aa5
Farros Mufid <farrosmufid.ofid@gmail.com>
2021-12-06 15:44:23 +0700

    add all files

---
+++ showAnswer.js
@@ -0,0 +1,18 @@
+function showAnswer() {
+    var x = document.getElementById("jokeAnswer");
+
+
+    var btnId = document.getElementById("myButton");
+
+    if (btnId.innerHTML == "Click to show answer") {
+        btnId.innerHTML = "Click to hide answer";
+    } else {
+        btnId.innerHTML = "Click to show answer";
+    }
+
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
